2020245107734a9e207b5eba01ff3d81257c1073
kangyonggan <kangyonggan@gmail.com>
2018-03-27 18:08:57 +0800

    更新章节的任务

--- blog-biz/src/main/java/com/kangyonggan/blog/engine/SectionEngine.java
+++ blog-biz/src/main/java/com/kangyonggan/blog/engine/SectionEngine.java
@@ -1,6 +1,7 @@
 package com.kangyonggan.blog.engine;
 
 import com.kangyonggan.blog.service.NovelService;
+import com.kangyonggan.blog.service.SectionService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,22 +11,22 @@ import org.springframework.stereotype.Component;
  * @author kangyonggan
  * @since 3/25/18
  */
-//@Component
+@Component
 @Log4j2
-public class NovelEngine {
+public class SectionEngine {
 
     @Autowired
-    private NovelService novelService;
+    private SectionService sectionService;
 
     /**
-     * 每天凌晨1点更新小说
+     * 每天凌晨1点更新章节
      * cron表达式：* * * * * *（秒 分 时 日 月 星期）
      */
     @Scheduled(cron = "0 0 1 * * *")
     public void execute() {
-        log.info("引擎自动更新小说开始...");
-        novelService.updateNovelFromNow(novelService.findLastNovelCode());
-        log.info("引擎自动更新小说结束!");
+        log.info("引擎自动更新章节开始...");
+        sectionService.updateAllSections();
+        log.info("引擎自动更新章节结束!");
     }
 
 }
